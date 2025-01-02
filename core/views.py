b807da575de359ab9fcc227f05185e54b92112a3
Eltaf-azizi <iltfhuss@gmail.com>
2025-01-02 21:29:15 +0500

    added checkout.html file

--- core/views.py
+++ core/views.py
@@ -5,3 +5,14 @@ def item_list(request):
         'items': Item.objects.all()
     }
     return render(request, "home-page.html", context)
+
+
+def checkout(request):
+    return render(request, "checkout.html")
+
+
+def home(request):
+    context = {
+        'items': Item.objects.all()
+    }
+    return render(request, "home.html", context)
