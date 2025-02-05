a3dbd41f54cf1d27e2382c6f13f90de5019ec31d
Eltaf-azizi <iltfhuss@gmail.com>
2025-02-05 22:00:07 +0500

    creating an order summary

--- core/models.py
+++ core/models.py
@@ -64,6 +64,16 @@ class OrderItem(models.Model):
     def get_total_discount_item_price(self):
         return self.quantity * self.item.price
 
+    
+    def get_amount_saved(self):
+        return self.get_total_item_price() - self.get_total_discount_item_price()
+    
+
+    def get_final_price(self):
+        if self.item.discount_item_price:
+            return self.get_total_discount_item_price()
+        return self.get_total_item_price()
+
 
 class Order(models.Model):
     user = models.foreignKey(settings.AUTH_USER_MODEL,
@@ -75,3 +85,11 @@ class Order(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+    def get_total(self):
+        total = 0
+        for order_item in self.items.all():
+            total += order_item.get_final_price()
+
+        return total
